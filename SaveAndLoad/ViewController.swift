d708a8dd9bdfdbd6bfe75b66e0771db495533afc
Aaron Bradley <abeats20@gmail.com>
2015-04-17 19:09:34 -0500

    initial commit. setting up basic methods for saving/loading

--- SaveAndLoad/ViewController.swift
+++ SaveAndLoad/ViewController.swift
@@ -8,12 +8,81 @@
 
 import UIKit
 
+// Documents Directory  /User/aaronb/.../Documents/
+func documentsDirectory() -> String {
+    let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as! String
+    return documentsFolderPath
+}
+
+func fileInDocumentsDirectory(fileName: String) -> String {
+    return documentsDirectory().stringByAppendingPathComponent(fileName)
+}
+
+// File in Documents Directory  /User/aaronb/.../Documents/Photo.png
+
+
+
 class ViewController: UIViewController {
+
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println("Documents: \(documentsDirectory())")
+
+        let imagePath = fileInDocumentsDirectory("Photo.jpg")
+        println("Photo path: \(imagePath)")
+
+        // Load image from Resource Bundle (read only)
+        var image = UIImage(named: "photo.png")
+        if image != nil {
+            // Save it to Documents folder
+            println(image)
+
+            let result = saveImage(image!, path: imagePath)
+            println("saveImage: \(result)")
+
+            // Load image
+            var loadedImage = loadImageFromPath(imagePath)
+            if loadedImage != nil {
+                println("Image loaded: \(loadedImage!)")
+
+                imageView.image = loadedImage
+            }
+        }
+
+        // Text Loading and Saving
+
     }
+
+    func saveImage(image: UIImage, path: String) -> Bool {
+        let jpgImageData = UIImagePNGRepresentation(image)
+        let result = jpgImageData.writeToFile(path, atomically: true)
+        return result
+    }
+
+    func loadImageFromPath(path: String) -> UIImage? {
+        let image = UIImage(contentsOfFile: path)
+        if image == nil {
+            println("No image here bruh: \(path)")
+        }
+        return image
+    }
+
+    // Save text
+    func saveText(text: String, path: String) {
+        var error: NSError? = nil
+        let status = text.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
+
+        if !status {
+            println("Error saving file at path: \(path) with error: \(error?.localizedDescription)")
+        }
+    }
+
+    // Load text
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
